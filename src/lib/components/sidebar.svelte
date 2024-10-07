5c91af6992df001292776ba43f3e14b865005fd7
Abhay <abhay1029singh@gmail.com>
2024-10-07 19:55:34 +0530

    fixed hidden content

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -26,7 +26,7 @@
 	<!--	<div>-->
 	<!--		class="sidebar  border-r {$sidebarStore ? 'absolute top-0 bottom-0 bg-white w-3/4 md:w-1/2' : 'hidden lg:flex'} lg:w-16 flex flex-col justify-start">-->
 	<div
-		class="sidebar z-50 border-r w-3/4 md:w-1/2 transition-all {$sidebarStore ? 'absolute top-0 bottom-0 bg-white translate-x-0' : 'hidden -translate-x-full lg:translate-x-0'} lg:w-16 lg:flex flex-col justify-start">
+		class="sidebar h-screen z-50 border-r w-3/4 md:w-1/2 transition-all {$sidebarStore ? 'absolute top-0 bottom-0 bg-white translate-x-0 md:relative' : 'hidden -translate-x-full lg:translate-x-0'} lg:w-16 lg:flex flex-col justify-start">
 		<div class="sidebar-title w-full px-4 lg:px-2 my-5 lg:my-0 flex gap-2 justify-start items-center">
 			<div class="flex items-center gap-2 rounded-full border-2 border-black p-3 my-4 bg-black">
 				<Logo width="24" height="24" className="w-5 h-5 md:w-4 md:h-4 text-white bg-black" />
@@ -50,7 +50,7 @@
 	<button
 		class="z-50 sidebar-blanket -translate-x-[400%] md:-translate-x-[200%] w-1/4 md:w-1/2 lg:hidden absolute top-0 left-auto right-0 bottom-0 bg-black/50 h-screen {$sidebarStore ? 'translate-x-0 md:translate-x-0' : 'hidden'}"
 		on:click={closeSidebar}></button>
-	<div class="sidebar-content w-full ">
+	<div class="sidebar-content w-full">
 		<slot />
 	</div>
 </section>
