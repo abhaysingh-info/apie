fff45c02317fbcff70e038edd32958e93742d5e1
Abhay <abhay1029singh@gmail.com>
2024-10-08 20:13:02 +0530

    bg changed

--- src/lib/components/connector-cards/connector-cards-grid.svelte
+++ src/lib/components/connector-cards/connector-cards-grid.svelte
@@ -7,7 +7,7 @@
 </script>
 
 
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+<div class="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
 	{#each connectors as connector}
 		<div>
 			<Connector {connector} />
